90c9759673b25b6e51f62ecf157fe84ae23c2d14
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2021-12-19 21:39:41 +0100

    Update e-steps

--- config/anycubic-vyper/Configuration.h
+++ config/anycubic-vyper/Configuration.h
@@ -924,7 +924,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 100, 400, 405}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 100, 400, 420 }
 
 /**
  * Default Max Feed Rate (mm/s)
